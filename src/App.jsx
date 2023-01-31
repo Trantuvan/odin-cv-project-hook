d5150430a2511d5cec8d4adab0925dcd28a565f8
trantuvan <trantuvan.kan@gmail.com>
2023-01-31 17:24:25 +0700

    add personal form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import clsx from 'clsx';
 import styles from './styles/App.module.css';
 import FormToolBar from './components/common/form-tool-bar';
+import FormPersonal from './components/personal-form/';
+import { useState } from 'react';
 
 function App() {
+  const [personal, setPersonal] = useState(() => ({
+    givenName: 'Vander',
+    familyName: 'Tran',
+    email: 'tran@gmail.com',
+    headline: 'Work Hard Play Hard',
+    phoneNumber: '0369158125',
+    address: '82 297 street Phuoc Long B ward, District 9',
+    postalCode: '70000',
+    city: 'Ho Chi Minh',
+    photo: '',
+  }));
+
+  function handlePersonChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPersonal({ ...personal, [name]: value });
+  }
+
   return (
     <div className={clsx(styles.grid2Cols)}>
       <div className={clsx(styles.formContainer)}>
-        <FormToolBar formName="Personal details" />
+        <div className={styles.formSection}>
+          <FormToolBar formName="Personal details" />
+          <FormPersonal form={personal} handleChange={handlePersonChange} />
+        </div>
+        <div className={styles.formSection}>
+          <FormToolBar formName="Personal details" />
+          <FormPersonal form={personal} handleChange={handlePersonChange} />
+        </div>
       </div>
       <div className={clsx(styles.resumeContainer)}>resume</div>
     </div>
---
+++ src/components/personal-form/FormPersonal.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { InputText } from '../common/form-controls';
+
+function FormPersonal({ form, handleChange }) {
+  return (
+    <form>
+      <InputText
+        labelName="Given Name"
+        inputId="given-name"
+        inputName="givenName"
+        form={form}
+        handleChange={handleChange}
+      />
+      <InputText
+        labelName="Family Name"
+        inputId="family-name"
+        inputName="familyName"
+        form={form}
+        handleChange={handleChange}
+      />
+      <InputText
+        labelName="Email Address"
+        inputId="email-address"
+        inputName="email"
+        form={form}
+        handleChange={handleChange}
+      />
+      <InputText labelName="Headline" inputId="headline" inputName="headline" form={form} handleChange={handleChange} />
+      <InputText labelName="Address" inputId="address" inputName="address" form={form} handleChange={handleChange} />
+      <InputText
+        labelName="Postal code"
+        inputId="postalCode"
+        inputName="postalCode"
+        form={form}
+        handleChange={handleChange}
+      />
+      <InputText labelName="City" inputId="city" inputName="city" form={form} handleChange={handleChange} />
+    </form>
+  );
+}
+
+FormPersonal.propTypes = {
+  form: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+export default FormPersonal;
