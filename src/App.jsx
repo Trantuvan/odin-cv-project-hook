aeb551fc3e64c2d76281e5998d9380f3c715191c
trantuvan <trantuvan.kan@gmail.com>
2023-01-31 14:53:09 +0700

    set grid2Cols layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import clsx from 'clsx';
+import styles from './styles/App.module.css';
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <div className={clsx(styles.grid2Cols)}>
+      <div className={clsx(styles.formContainer)}>forms</div>
+      <div className={clsx(styles.resumeContainer)}>resume</div>
+    </div>
+  );
 }
 
 export default App;
