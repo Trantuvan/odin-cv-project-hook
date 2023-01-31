7e42cbb238bddba18b74ff359df67dde701a4a09
trantuvan <trantuvan.kan@gmail.com>
2023-01-31 12:25:18 +0700

    settings for css module

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,22 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/odin-cv-project-hook/',
-  plugins: [react(), { ...eslint({ cache: true, fix: true }), apply: 'build' }],
-  server: {
-    open: true,
-    port: 3000,
-    host: true,
-  },
-});
+export default ({ mode }) => {
+  const generateScopedName = mode === 'production' ? '[hash:base64:2]' : '[local]_[hash:base64:2]';
+
+  return defineConfig({
+    base: '/odin-cv-project-hook/',
+    plugins: [react(), { ...eslint({ cache: true, fix: true }), apply: 'build' }],
+    server: {
+      open: true,
+      port: 3000,
+      host: true,
+    },
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+        generateScopedName: generateScopedName,
+      },
+    },
+  });
+};
