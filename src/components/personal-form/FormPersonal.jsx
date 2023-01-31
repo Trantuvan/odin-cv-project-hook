3a01cd638dcfbc64dff633b09938e5a38c259a03
trantuvan <trantuvan.kan@gmail.com>
2023-01-31 18:38:56 +0700

    done for personal form

--- src/components/personal-form/FormPersonal.jsx
+++ src/components/personal-form/FormPersonal.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Avatar, InputText } from '../common/form-controls';
+import styles from '../../styles/FormPersonal.module.css';
+import clsx from 'clsx';
 
 function FormPersonal({ form, handleChange, handlePreview }) {
   return (
     <form>
-      <Avatar form={form} handlePreview={handlePreview} />
-      <InputText
-        labelName="Given Name"
-        inputId="given-name"
-        inputName="givenName"
-        form={form}
-        handleChange={handleChange}
-      />
-      <InputText
-        labelName="Family Name"
-        inputId="family-name"
-        inputName="familyName"
-        form={form}
-        handleChange={handleChange}
-      />
-      <InputText
-        labelName="Email Address"
-        inputId="email-address"
-        inputName="email"
-        form={form}
-        handleChange={handleChange}
-      />
+      <div className={clsx(styles.gridAvatarInputs)}>
+        <Avatar form={form} handlePreview={handlePreview} />
+        <div className={clsx(styles.grid2ColsForms)}>
+          <InputText
+            labelName="Given Name"
+            inputId="given-name"
+            inputName="givenName"
+            form={form}
+            handleChange={handleChange}
+          />
+          <InputText
+            labelName="Family Name"
+            inputId="family-name"
+            inputName="familyName"
+            form={form}
+            handleChange={handleChange}
+          />
+          <InputText
+            labelName="Email Address"
+            inputId="email-address"
+            inputName="email"
+            form={form}
+            handleChange={handleChange}
+          />
+        </div>
+      </div>
       <InputText labelName="Headline" inputId="headline" inputName="headline" form={form} handleChange={handleChange} />
       <InputText labelName="Address" inputId="address" inputName="address" form={form} handleChange={handleChange} />
       <InputText
