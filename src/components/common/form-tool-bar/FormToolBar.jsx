77cf793261d7df12124fe6c58d1dad1612e798d2
trantuvan <trantuvan.kan@gmail.com>
2023-02-05 11:02:19 +0700

    add feature scroll smooth to formToolBar

--- src/components/common/form-tool-bar/FormToolBar.jsx
+++ src/components/common/form-tool-bar/FormToolBar.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { MdOutlineKeyboardArrowUp, MdOutlineKeyboardArrowDown } from 'react-icons/md';
@@ -9,6 +10,11 @@ import { useToggle } from '../../../hooks/useToggle';
 
 function FormToolBar({ formName, formNumber, isOpen }) {
   const { toggleForm1, toggleForm2, toggleForm3 } = useToggle();
+  const ref = useRef(null);
+
+  function handleScroll() {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  }
 
   function handleToggle() {
     switch (formNumber) {
@@ -28,10 +34,17 @@ function FormToolBar({ formName, formNumber, isOpen }) {
         throw new Error('Unsupported form number:' + formName);
     }
   }
+
   return (
-    <div className={clsx(styles.formToolbarContainer)}>
+    <div className={clsx(styles.formToolbarContainer)} ref={ref}>
       <h1 className={clsx(styles.formToolbarTitle)}>{formName}</h1>
-      <div className={clsx(styles.formToolbarAction)} onClick={() => handleToggle()}>
+      <div
+        className={clsx(styles.formToolbarAction)}
+        onClick={() => {
+          handleToggle();
+          handleScroll();
+        }}
+      >
         {isOpen ? (
           <MdOutlineKeyboardArrowUp className={clsx(styles.btnAction)} />
         ) : (
